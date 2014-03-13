10112055ccc80232e5c3b8c29a6141875c60241c
kylinpoet <kylinpoet@gmail.com>
2014-03-13 11:21:16 +0800

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -6,13 +6,13 @@
 
 from xgoogle.search import GoogleSearch, SearchError
 try:
-  gs = GoogleSearch("quick and dirty")
+  gs = GoogleSearch("kylinpoet")
   gs.results_per_page = 50
   results = gs.get_results()
   for res in results:
-    print res.title.encode('utf8')
-    print res.desc.encode('utf8')
-    print res.url.encode('utf8')
+    print res.title.encode('gbk')
+    print res.desc.encode('gbk')
+    print res.url.encode('gbk')
     print
 except SearchError, e:
   print "Search failed: %s" % e
